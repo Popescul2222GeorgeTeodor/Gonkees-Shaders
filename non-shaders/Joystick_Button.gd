ad97ff15eba09be882dc2c14651f91e4386d6d57
document10 <popescul2222georgeteodor@gmail.com>
2021-05-31 23:10:47 +0300

    Update Joystick_Button.gd

--- non-shaders/Joystick_Button.gd
+++ non-shaders/Joystick_Button.gd
@@ -1,19 +1,29 @@
 extends TouchScreenButton
 
 # Gonkee's joystick script for Godot 3 - full tutorial https://youtu.be/uGyEP2LUFPg
-# If you use this script, I would prefer if you gave credit to me and my channel
+# If you use this script, I would prefer if you gave credit to me and gonkee
 
 # Change these based on the size of your button and outer sprite
-var radius = Vector2(32, 32)
-var boundary = 64
-
+#Joystick's handle radius,keep same value on both coordinates
+export(Vector2) var radius = Vector2(32,32)
+#max distance for the handle to move 
+export(int) var boundary = 64
+#keeps track of current drag,-1 means nothing's touching the screen
 var ongoing_drag = -1
-
-var return_accel = 20
-
-var threshold = 10
-
+#at what speed the handle will return to center
+export(int) var return_accel = 20
+#enable this to see if the joystick is working
+#WARNING,your console will get spammed with info
+export(bool) var debugging = false;
+#how much we have to move joystick to register movement
+#values lower than this will be ignored
+export(int) var threshold = 10
+#if enabled,the length of the movement vector will not be constant
+#basically an analog controller
+export(bool) var analog = false 
 func _process(delta):
+	if debugging:
+		print(get_value())
 	if ongoing_drag == -1:
 		var pos_difference = (Vector2(0, 0) - radius) - position
 		position += pos_difference * return_accel * delta
@@ -36,7 +46,17 @@ func _input(event):
 	if event is InputEventScreenTouch and !event.is_pressed() and event.get_index() == ongoing_drag:
 		ongoing_drag = -1
 
+#this is what you use to move the character
 func get_value():
 	if get_button_pos().length() > threshold:
-		return get_button_pos().normalized()
+		if analog:
+			return get_button_pos()
+		else:
+			return get_button_pos().normalized()
 	return Vector2(0, 0)
+
+#use this for aiming using the joystick
+#make sure to use rotation not rotation_degrees
+func get_aim():
+	var dir= get_value();
+	return atan2(dir.y,dir.x);
